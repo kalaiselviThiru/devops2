e0515f34de97614b77cf5fe850e510300ca25927
kalaiselviThiru <kkalaiselvi011325@gmail.com>
2025-03-06 21:51:46 +0530

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,1 +1,20 @@
+// Calculator.java
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Addition: " + calculator.add(5, 3));
+        System.out.println("Subtraction: " + calculator.subtract(5, 3));
+    }
+}
+
 
